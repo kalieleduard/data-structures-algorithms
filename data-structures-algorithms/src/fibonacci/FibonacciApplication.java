173bb124ad541bf1eb74628e0f79c7588b1d7aff
Kaliel <kalieleduard1@gmail.com>
2023-12-08 08:09:58 -0300

    <feat>[algorithm]: <fibonacci with dynamic proggraming>

---
+++ data-structures-algorithms/src/fibonacci/FibonacciApplication.java
@@ -0,0 +1,8 @@
+package fibonacci;
+
+public class FibonacciApplication {
+    public static void main(String[] args) {
+        final var solution = new Solution();
+        System.out.println(solution.fib(10000));
+    }
+}
---
+++ data-structures-algorithms/src/fibonacci/Solution.java
@@ -0,0 +1,16 @@
+package fibonacci;
+
+public class Solution {
+
+    public long fib(int n) {
+        long[] fibArray = new long[n + 2];
+        fibArray[0] = 0;
+        fibArray[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            fibArray[i] = fibArray[i - 1] + fibArray[i - 2];
+        }
+
+        return fibArray[n];
+    }
+}
